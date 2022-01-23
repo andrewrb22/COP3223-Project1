2ff4cfb948fa7b5e8eafaf9c6bb991cd3f23f145
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-23 17:29:49 -0500

    first test with studentId

--- Project-1.c
+++ Project-1.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main() {
 
-int month, day , year; 
- 
-    printf ("Enter you date of birth in this format (month/day/year) :"); 
-    scanf("%d/%d/%d", &month, &day, &year); 
- 
-    printf("Alright! you were born on %d-%d-%d\n\n", month, day, year); 
- 
- 
- 
-    printf ("Now Enter you date of birth in this format (month*day*year) :"); 
-    scanf("%d  %d  %d", &month, &day, &year); 
- 
-    printf("Cool! you were born on %d/%d/%d\n\n", month, day, year); 
- 
-    printf(":)"); 
+int studentid;
+int course;
+int credits;
+float total;
+int fee = 35;
 
+printf("Enter the Students Id:");
+scanf("%d", &studentid);
+printf("Your student id from valencia is:V%d\n ", studentid);
 
 
     return 0; 
