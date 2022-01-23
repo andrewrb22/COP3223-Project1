46ad73cd6043adb9e8ba84d170a4a54d00c3bc2b
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-23 16:40:14 -0500

    testing code from project description

--- Project-1.c
+++ Project-1.c
@@ -3,7 +3,21 @@
 
 int main() {
 
-
+int month, day , year; 
+ 
+    printf ("Enter you date of birth in this format (month/day/year) :"); 
+    scanf("%d/%d/%d", &month, &day, &year); 
+ 
+    printf("Alright! you were born on %d-%d-%d\n\n", month, day, year); 
+ 
+ 
+ 
+    printf ("Now Enter you date of birth in this format (month*day*year) :"); 
+    scanf("%d  %d  %d", &month, &day, &year); 
+ 
+    printf("Cool! you were born on %d/%d/%d\n\n", month, day, year); 
+ 
+    printf(":)"); 
 
 
 
