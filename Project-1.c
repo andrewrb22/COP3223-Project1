46b48353c2a37e0e32457a79f965d36ed80482da
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-23 17:58:26 -0500

    Enter and scan course and credit number

--- Project-1.c
+++ Project-1.c
@@ -5,15 +5,21 @@
 
 int main() {
 
-int studentid;
+int studentId;
 int course;
 int credits;
 float total;
 int fee = 35;
 
+//Enter and scan Student ID
 printf("Enter the Students Id:");
-scanf("%d", &studentid);
-printf("Your student id from valencia is:V%d\n ", studentid);
+scanf("%d", &studentId);
+
+//Enter and scan Course and credits
+printf("Enter crn/credit hours for first course (like 5665/3):");
+scanf("%d/%d", &course, &credits);
+
+printf("Student ID %d CRN %d and credits %d", studentId,course,credits);
 
 
     return 0; 
