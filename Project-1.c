f1f0f29073b7c56828f50550a7a1e12998a57e8d
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-23 19:10:20 -0500

    added comments to code

--- Project-1.c
+++ Project-1.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 
-
 int main() {
 
 int studentId;
@@ -27,16 +26,18 @@ scanf("%d/%d", &course1, &credits1);
 printf("Enter course/credit hours for second course (like 5665/3):");
 scanf("%d/%d", &course2, &credits2);
 
+//Total for first course
 total1 = credits1 * 120.25 ;
+
+//Total for second course
 total2 = credits2 * 120.25;
+
+//Final total of invoice 
 paymenttotal = total1 + total2 + fee;
 
+//Print Invoice for student
 printf(" \n VALENCE COMMUNITY COLLEGE \n ORLANDO FL 10101 \n ************************* \n Fee Invoice Prepared for Student V-%d \n\n 1 Credit Hour = $120.25 \n\n CRN \t\t CREDIT HOURS \t\t Total",studentId);
 printf("\n %d \t\t %d\t\t\t $%.2f \n %d \t\t %d\t\t\t $%.2f\n\n \t\tHealth & id fees\t$35.00\n\n--------------------------------------\n\n\t\t Total Payments\t $%.2f\n", course1,credits1,total1,course2,credits2,total2,paymenttotal);
-
-
-
-
 
     return 0; 
 }
